a0d9e40a9126da7935760f5cc2341b50efc1ed3b
Frooxius <tomas.mariancik@gmail.com>
2026-09-01 21:06:23 +0200

    tweak: Mark submesh topology as data model type

--- Renderite.Shared/Assets/Meshes/SubmeshTopology.cs
+++ Renderite.Shared/Assets/Meshes/SubmeshTopology.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Elements.Data;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Renderite.Shared
 {
+    [DataModelType]
     public enum SubmeshTopology
     {
         Points,
